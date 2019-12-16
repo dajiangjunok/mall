1b2d0bb570d7b0c69a368325ac4a61d01a46c44f
codewyj <779503577@qq.com>
2019-12-16 18:25:12 +0800

    第三次提交

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 6px 1px rgba(58, 58, 58, 0.3);
+}
+.nav-bar .left {
+  width: 60px;
+}
+.nav-bar .center {
+  flex: 1;
+}
+.nav-bar .right {
+  width: 60px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,40 @@
 <template>
-  <h2>我是首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>购物街</template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-export default {}
+import NavBar from 'components/common/navbar/NavBar'
+import { getHomeMultidata } from 'network/home'
+export default {
+  name: 'Home',
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  methods: {},
+  components: {
+    NavBar
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      console.log(res)
+      this.banners = res.data.banner
+      this.recommends = res.data.recommend
+    })
+  }
+}
 </script>
 
 <style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
